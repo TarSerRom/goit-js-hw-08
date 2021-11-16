2ab60f16a03292aace4a5336bed1dc7eabef00a9
Sergey Tarasov <tarasovsergeyrom@gmail.com>
2021-11-16 11:49:46 +0200

    Homework 8 ver.1( 1st task)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryItems(galleryItems);
+
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+galleryContainer.addEventListener('click', onItemClick);
+
+function createGalleryItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `
+    }).join('');
+       
+}
+
+function onItemClick(e) {
+    e.preventDefault();
+
+    console.log(e.target);
+}
+
+let gallery = new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay: 250 });
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
