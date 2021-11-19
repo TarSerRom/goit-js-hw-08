4a1b59bd3c3ff7f29fb3043f722ccf4a3b649666
TarSerRom <87226909+TarSerRom@users.noreply.github.com>
2021-11-19 22:10:22 +0000

    Deploying to gh-pages from @ TarSerRom/goit-js-hw-08@dde09022f086b21f4359020042e6e2af08b4597a 🚀

--- 01-gallery.b91e2319.js
+++ 01-gallery.b91e2319.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=require("./gallery-items"),l=t(require("simplelightbox"));function t(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css"),console.log(e.galleryItems);const i=document.querySelector(".gallery"),r=a(e.galleryItems);function a(e){return e.map(({preview:e,original:l,description:t})=>`\n    <a class="gallery__item" href="${l}">\n  <img class="gallery__image" src="${e}" alt="${t}" />\n</a>\n    `).join("")}function n(e){e.preventDefault()}i.insertAdjacentHTML("beforeend",r),i.addEventListener("click",n);let s=new l.default(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/goit-js-hw-08/01-gallery.835fc0cf.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.b91e2319.js.map
